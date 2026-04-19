c6213d04d183049283a2d89a1ae0251fb75bfabf
Amit Paudel <amitpaudel789@gmail.com>
2026-04-19 18:50:16 +0545

    active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+'use client';
 import Link from 'next/link';
 import { AiFillBug } from 'react-icons/ai';
 import React from 'react';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const links = [
   { label: 'Dashboard', href: '/' },
@@ -8,22 +11,35 @@ const links = [
 ];
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
   return (
     <nav className="flex justify-around m-4 items-center h-14  ">
       <Link href="/">
         <AiFillBug size="25" />
       </Link>
 
-      <ul className="space-x-4">
+      <ul className="space-x-8">
         {links.map((link) => {
           return (
             <Link
               key={link.href}
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                'text-zinc-900': currentPath == link.href,
+                'text-zinc-500': currentPath !== link.href,
+                'hover:text-zinc-800 transition-colors': true,
+              })}
             >
               {link.label}
             </Link>
+            // <Link
+            //   key={link.href}
+            //   href={link.href}
+            //   className={`${link.href == currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+            // >
+            //   {link.label}
+            // </Link>
           );
         })}
       </ul>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>IssuePage</div>;
+};
+
+export default page;
