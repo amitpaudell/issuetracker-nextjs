1ef64f59841d3a237f678d8728bb00da9399a586
Amit Paudel <amitpaudel789@gmail.com>
2026-05-02 14:05:12 +0545

    building charts and layout dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from 'next/image';
-import Pagination from './components/Pagination';
+import { prisma } from '@/lib/prisma';
+import IssueSummary from './IssueSummary';
+import LatestIssue from './LatestIssue';
+import IssueChart from './IssueChart';
+import { Flex, Grid } from '@radix-ui/themes';
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
   return (
-    <div>
-      <Pagination itemCount={200} pageSize={10} currentPage={2}></Pagination>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssue></LatestIssue>
+    </Grid>
   );
 }
